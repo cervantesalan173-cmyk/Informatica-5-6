52d6e9a313d75c33b29162d9bf0012907f067785
cervantesalan173-cmyk <cervantesalan173@gmail.com>
2026-08-18 21:58:04 +0000

    hola

--- currency.py
+++ currency.py
@@ -1,9 +1,10 @@
 def main():
-    pesos = input("What do you have left in pesos? ")
-    soles = input("What do you have left in soles? ")
-    reais = input("What do you have left in reais? ")
-    usd = pesos*0.00032 + soles*0.30+ reais*0.19
-    print(f"USD:{usd}")
-
+    pesos = float(input("What do you have left in pesos? "))
+    soles = float(input("What do you have left in soles? "))
+    reais = float(input("What do you have left in reais? "))
+    usd = pesos*0.00032 + soles*0.30 + reais*0.19
+    mxn = pesos*183.87 + soles/0.20 + reais/0.31
+    print(f"USD:{round(usd,2)}")
+    print(f"mxn:{round(mxn,2)}")
 if __name__ == "__main__":
     main()

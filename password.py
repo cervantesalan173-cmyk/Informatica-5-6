052c4321368c06cfb3a3c54cba97281bc3519606
cervantesalan173-cmyk <cervantesalan173@gmail.com>
2026-08-24 15:00:24 +0000

    hola

---
+++ password.py
@@ -0,0 +1,13 @@
+def main():
+password = "alan1234"
+
+attempt = input("enter your password: ")
+if  attempt == password:
+    print("Correct password. ")
+
+print("see you later. ")
+
+
+
+if __name__ == "__main__":
+    main()

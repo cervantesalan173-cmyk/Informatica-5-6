774a4a8d0ec4848e340d821220130d68bdb5f43d
cervantesalan173-cmyk <cervantesalan173@gmail.com>
2026-08-25 20:20:38 +0000

    hola

---
+++ rating.py
@@ -0,0 +1,14 @@
+def main():
+    print("Los pollos hermanos family!")
+
+    rating = float(input("rate our service from 0 to 5!: "))
+    if rating >= 4.5:
+        print("Perfetcion!")
+    elif 4.5 > rating > 4:
+        print("Excellent!")
+
+
+
+
+if __name__ == "__main__":
+    main()

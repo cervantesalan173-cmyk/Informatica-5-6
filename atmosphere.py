926999f24a40a2c83a74eb0d42bf2ed652f616b6
cervantesalan173-cmyk <cervantesalan173@gmail.com>
2026-08-27 15:46:21 +0000

    hola

--- atmosphere.py
+++ atmosphere.py
@@ -1,12 +1,27 @@
 def main():
     layer = input("choose one layer in earth´s atmosphere: ")
-    Exosphere = input("your altittude level wil be between 700–10,000 km")
-    Thermosphere = input("your altittude level will be between 85–700 km")
+    Exosphere = input("700 10,000 km.")
+    Thermosphere = input("85–700 km")
     Mesosphere = input("your altittude level will be between 50–85 km")
     Stratosphere = input("your altittude level will be between 12–50 km")
     Troposphere = input("your altittude lvel will be between 0–12 km")
-    altittude = int(input("enter exact altittude: "))
+    altittude = input("enter exact altittude: ")
+    print(layer)
+    print(f"Your altittude:"(Exosphere))
+    print(f"Your altittude:"(Thermosphere))
+    
 
+    #print(f"your altittude level will be between{Exosphere}.")
+
+    #print(f"your altittude level will be between{Thermosphere}.")
+
+
+    #print()
+
+    #if Exosphere:
+     #   print(Exosphere)
+    #elif Thermosphere:
+     #   print(Thermosphere)
 
 
 

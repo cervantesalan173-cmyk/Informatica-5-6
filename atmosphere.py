bb7826b7504f4046fb1bbbe2b96ca9a4dec9c79c
cervantesalan173-cmyk <cervantesalan173@gmail.com>
2026-08-27 14:58:44 +0000

    hopij

---
+++ atmosphere.py
@@ -0,0 +1,16 @@
+def main():
+    layer = input("choose one layer in earth´s atmosphere: ")
+    Exosphere = input("your altittude level wil be between 700–10,000 km")
+    Thermosphere = input("your altittude level will be between 85–700 km")
+    Mesosphere = input("your altittude level will be between 50–85 km")
+    Stratosphere = input("your altittude level will be between 12–50 km")
+    Troposphere = input("your altittude lvel will be between 0–12 km")
+    altittude = int(input("enter exact altittude: "))
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()

a522ac6208e9be89b7b52b6e3d3e900506917f09
cervantesalan173-cmyk <cervantesalan173@gmail.com>
2026-09-01 20:20:20 +0000

    hola

---
+++ coinflip.py
@@ -0,0 +1,16 @@
+import random
+def main():
+    side = input("Guess the landing side: ")
+    flip = random.randint(1, 2)
+    if side == 1
+        print("correct")
+    elif side == 2
+        print("correct")
+
+
+
+
+if __name__=="__main__":
+    main()
+
+
---
+++ monopoly.py
@@ -0,0 +1,8 @@
+import random
+
+def main():
+    die1 = random.randomint(1, 6)
+    die2 = random.randomint(1,6)
+    print(die1 + die2)
+
+    #figures

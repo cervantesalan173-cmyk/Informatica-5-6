5e1d9593f5f5ae4237718c524ffa62d67523cf38
cervantesalan173-cmyk <cervantesalan173@gmail.com>
2026-09-15 15:50:36 +0000

    thsf

---
+++ Radio-playlist.py
@@ -0,0 +1,33 @@
+import time
+def main():
+    playlist = ["Boston", "Dracula", "I knew it, I knew you", "hate that I made you love me", "Risk It all"]
+
+    playlist.append("Be By You")
+
+    playlist.insert(0, "Bohemian Rhapsody")
+
+    playlist.pop(4)
+    print(playlist)
+    print(playlist.index("Risk It all"))
+
+    print("Number of songs in playlist:", len(playlist))
+
+    playlist.reverse()
+    print(playlist)
+    playlist.sort()
+    print(playlist)
+
+    #Challenge
+    repeat = len(playlist)
+    while repeat > 0:
+        print(playlist)
+        song = playlist[0]
+        playlist.pop(0)
+        playlist.append(song)
+        repeat -= 1
+        time.sleep(3)
+
+
+
+if __name__ == "__main__":
+    main()

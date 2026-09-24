620d0e95fe4406b246d1403a564ac4f73c85604f
cervantesalan173-cmyk <cervantesalan173@gmail.com>
2026-09-24 15:32:33 +0000

    alatorre

--- weekly 6/tables.py
+++ weekly 6/tables.py
@@ -1,11 +1,29 @@
 def main():
+    valid_nums = []
+    for i in range(1,11):
+        valid_nums.append(str(i))
+
     while True:
-        number = int(input("select a number from 1-10: "))
-        if number <= 10:
-            if number > 0:
-                    for i in range(10):
-                       tatortot = i * number
+        number = input("select a number from 1-10 or exit: ").lower().strip()
+
+        if number == "exit":
+             break
+
+        elif number in valid_nums:
+            max_value = int(input("Enter maximum value for the times table: "))
+
+
+            print(f"Here is the {number} times table")
+
+
+            for i in range(1, max_value + 1):
+                       tatortot = i * int(number)
                        print(f"{i} times {number} is {tatortot}")
+
+            else:
+                 print("Invalid command.")
+
+
 
 if __name__ =="__main__":
     main()
